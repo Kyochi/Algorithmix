598d2ac01463109f50dabf53247684b5167b39a9
Kyochi <baptistech@hotmail.fr>
2016-08-07 21:10:28 +0200

    Updated

--- Algorithmix/Algo.cs
+++ Algorithmix/Algo.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithmix
 {
@@ -35,6 +31,44 @@ namespace Algorithmix
                 }
             }
             Console.WriteLine(" Fini !");
+        }
+
+        /*
+         * 
+         * Problem : determine if any 3 integers in an array sum to 0. 
+         * Complexity : O(n²)
+         */
+        public static void threeSumV1(int [] tab)
+        {
+            // quicksort : O(n log n)
+            Array.Sort<int>(tab);
+
+            for (int i = 0; i != tab.Length; i++)
+            {
+                int k = 0;
+                int j = tab.Length-1;
+                while ( k < j && k < tab.Length && j < tab.Length && j > 0)
+                {
+
+                    if (k == i) {
+                        k++;
+                        continue;
+                    }
+
+                    if (j == i) {
+                        j--;
+                        continue;
+                    }
+
+                    if (tab[i] == (tab[k] + tab[j]))
+                    {
+                        Console.WriteLine("Finded : " + tab[i] + " : " + tab[k] + " : " + tab[j]);
+                    }
+
+                    if (tab[k] > tab[i]) j--;
+                    else k++;
+                }
+            }
         }
     }
 }
--- Algorithmix/Program.cs
+++ Algorithmix/Program.cs
@@ -10,21 +10,29 @@ namespace Algorithmix
     {
         static void Main(string[] args)
         {
+
+            Algo.threeSumV1(new int[] { -10, -25, 0, 1, 2 });
+        }
+
+        /*
+         * Test combinaison problem
+         */ 
+        public void testCombinaisonStep()
+        {
             var watch = System.Diagnostics.Stopwatch.StartNew();
             Console.WriteLine(DynamicProg.BruteCountCombinaisonStep(30));
             watch.Stop();
             var elapsedMs = watch.ElapsedTicks;
             Console.WriteLine("Time 1 : " + elapsedMs);
-            
-            
-            
+
+
+
             var watch2 = System.Diagnostics.Stopwatch.StartNew();
             Console.WriteLine(DynamicProg.callCountComb(30));
             watch2.Stop();
             var elapsedMs2 = watch2.ElapsedTicks;
 
             Console.WriteLine("Time 2 : " + elapsedMs2);
-            
         }
     }
 }
